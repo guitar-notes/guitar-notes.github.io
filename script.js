e2c2b056d30f2fa203b132ed4d075ab52507d26d
chiayewken <chiayewken@gmail.com>
2024-09-09 23:33:34 +0800

    Add button to play random two notes (in the top strings)

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let objects = {};
+let randomIndex = -1;
+let randomIndex2 = -1;
 
 function playAudio(source) {
   if (!objects[source]) {
@@ -15,4 +17,36 @@ function playAudio(source) {
     // If audio is not playing, start playback
     audio.play();
   }
+}
+
+function makeSublist(list, i, offset) {
+    let sublist = [];
+    for (let j = i - offset; j <= i + offset; j++) {
+        if (j >= 0 && j < list.length && j !== i) {
+            sublist.push(list[j]);
+        }
+    }
+    return sublist;
+}
+
+function playRandom(audioFiles) {
+    if (audioFiles.length === 0) {
+        console.log("No audio files provided");
+        return;
+    }
+
+    if (randomIndex === -1) {
+        randomIndex = Math.floor(Math.random() * audioFiles.length);
+    }
+
+    sublist = makeSublist(audioFiles, randomIndex, 3);
+    if (randomIndex2 === -1) {
+        randomIndex2 = Math.floor(Math.random() * sublist.length);
+    }
+    playAudio(audioFiles[randomIndex]);
+
+    // wait for 1 second
+    setTimeout(() => {
+        playAudio(sublist[randomIndex2]);
+    }, 1000);
 }
